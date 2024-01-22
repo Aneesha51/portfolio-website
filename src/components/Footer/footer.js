b8b87ab8b218c7fd607813731cb79cd187863938
Aneesha <69849089+Aneesha51@users.noreply.github.com>
2024-01-22 21:09:58 +0200

    Fixed a spelling mistake

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer className='footer'>
-        Copyright &#169; 2024 Aneeesha Rangan. All rights received.
+        Copyright &#169; 2024 Aneesha Rangan. All rights received.
     </footer>
   )
 }
